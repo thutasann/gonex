bbe505b065eab1bfe574baae48ac14741b50c0d1
thutasann <thutasann2002@gmail.com>
2025-07-31 19:53:24 +0800

    feat(core): context

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,15 @@
+/**
+ * A generic type alias for any value.
+ *
+ * @remarks
+ * Use this when the type is unknown or dynamic and strict typing isn't possible or necessary.
+ * Prefer using more specific types when feasible.
+ *
+ * @example
+ * let data: AnyValue;
+ * data = 42;
+ * data = "hello";
+ * data = { key: "value" };
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyValue = any;
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export * from './channel';
+export * from './context';
 export * from './goroutine';
 export * from './mutex';
 export * from './once';
